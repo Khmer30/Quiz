2378c9b0f36b0295139a4c441e0862aeaa25df59
marie <dul.sreyneang@gmail.com>
2022-03-03 17:55:10 +0800

    Question and Answer
    add vertical stack and four buttons

---
+++ PersonalityQuiz/QuestionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  QuestionViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Joy Marie on 3/3/22.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ PersonalityQuiz/ResultsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ResultsViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Joy Marie on 3/3/22.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
