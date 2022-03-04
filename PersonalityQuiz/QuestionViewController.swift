7d54abc2e5053d15f7b6f08eec9a88db703e04db
marie <dul.sreyneang@gmail.com>
2022-03-04 16:57:07 +0800

    isHidden
    add isHidden boolean for each stack views

--- PersonalityQuiz/QuestionViewController.swift
+++ PersonalityQuiz/QuestionViewController.swift
@@ -40,23 +40,26 @@ class QuestionViewController: UIViewController {
                     Answer(text: "Fish", type: .cat),
                     Answer(text: "Carrots", type: .rabbit),
                     Answer(text: "Corn", type: .turtle)
-                 ]),
-          Question(text: "Which activities do you enjoy?",
+                 ]
+        ),
+        Question(text: "Which activities do you enjoy?",
                    type: .multiple,
                    answers: [
                     Answer(text: "Swimming", type: .turtle),
                     Answer(text: "Sleeping", type: .cat),
                     Answer(text: "Cuddling", type: .rabbit),
                     Answer(text: "Eating", type: .dog)
-                   ]),
-          Question(text: "How much do you enjoy car rides?",
+                   ]
+        ),
+        Question(text: "How much do you enjoy car rides?",
                    type: .ranged,
                    answers: [
                     Answer(text: "I dislike them", type: .cat),
                     Answer(text: "I get a little nervous", type: .rabbit),
                     Answer(text: "I barely notice them", type: .turtle),
                     Answer(text: "I love them", type: .dog)
-                   ])
+                   ]
+        )
     ]
     
     override func viewDidLoad() {
@@ -68,5 +71,19 @@ class QuestionViewController: UIViewController {
         singleStackView.isHidden = true
         multipleStackView.isHidden = true
         rangedStackView.isHidden = true
+    
+        navigationItem.title = "Question #\(questionIndex + 1)"
+        
+        let currentQuestion = questions[questionIndex]
+        
+        switch currentQuestion.type {
+        case .single:
+            singleStackView.isHidden = false
+        case .multiple:
+            multipleStackView.isHidden = false
+        case .ranged:
+            rangedStackView.isHidden = false
+        }
     }
 }
+var questionIndex = 0
