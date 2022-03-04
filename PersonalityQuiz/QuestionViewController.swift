7b8f12a9788cea6bb5e648f9ef5953c15c1d097c
marie <dul.sreyneang@gmail.com>
2022-03-04 16:19:33 +0800

    Models
    Add new file "Question"
    Create 2 Structure for Question to house the model definition and array of Answer Objects

--- PersonalityQuiz/QuestionViewController.swift
+++ PersonalityQuiz/QuestionViewController.swift
@@ -8,8 +8,65 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
-
+    
+    @IBOutlet var questionLabel: UILabel!
+    
+    @IBOutlet var singleStackView: UIStackView!
+    
+    @IBOutlet var singleButton1: UIButton!
+    @IBOutlet var singleButton2: UIButton!
+    @IBOutlet var singleButton3: UIButton!
+    @IBOutlet var singleButton4: UIButton!
+    
+    @IBOutlet var multipleStackView: UIStackView!
+    
+    @IBOutlet var multiLabel1: UILabel!
+    @IBOutlet var multiLabel2: UILabel!
+    @IBOutlet var multiLabel3: UILabel!
+    @IBOutlet var multiLabel4: UILabel!
+    
+    @IBOutlet var rangedStackView: UIStackView!
+    
+    @IBOutlet var rangedLabel1: UILabel!
+    @IBOutlet var rangedLabel2: UILabel!
+    
+    @IBOutlet var questionProgressView: UIProgressView!
+    
+    var questions: [Question] = [
+        Question(text: "Which food do you like the most?",
+                 type: .single,
+                 answers: [
+                    Answer(text: "Steak", type: .dog),
+                    Answer(text: "Fish", type: .cat),
+                    Answer(text: "Carrots", type: .rabbit),
+                    Answer(text: "Corn", type: .turtle)
+                 ]),
+          Question(text: "Which activities do you enjoy?",
+                   type: .multiple,
+                   answers: [
+                    Answer(text: "Swimming", type: .turtle),
+                    Answer(text: "Sleeping", type: .cat),
+                    Answer(text: "Cuddling", type: .rabbit),
+                    Answer(text: "Eating", type: .dog)
+                   ]),
+          Question(text: "How much do you enjoy car rides?",
+                   type: .ranged,
+                   answers: [
+                    Answer(text: "I dislike them", type: .cat),
+                    Answer(text: "I get a little nervous", type: .rabbit),
+                    Answer(text: "I barely notice them", type: .turtle),
+                    Answer(text: "I love them", type: .dog)
+                   ])
+    ]
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+    super.viewDidLoad()
+    updateUI()
+    }
+    
+    func updateUI() {
+        singleStackView.isHidden = true
+        multipleStackView.isHidden = true
+        rangedStackView.isHidden = true
     }
 }
--- PersonalityQuiz/ResultsViewController.swift
+++ PersonalityQuiz/ResultsViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+//    var respones: [Answer]
+//    
+//    init?(coder: NSCoder, responses: [Answer]) {
+//        self.respones = responses
+//        super.init(coder: coder)
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
